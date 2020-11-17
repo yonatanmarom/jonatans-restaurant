e002ae5f80190f6652cba6a61e874a56f3989551
yonatanmarom <yonatan.ma.100@gmail.com>
2020-11-17 21:15:47 +0200

    css

--- eee.js
+++ eee.js
@@ -1,25 +1,29 @@
 var el01 = document.querySelector('.cube1')
-el01.innerHTML = `<h1> menu </h1>`
-el01.innerHTML += `<button onClick="inputNumber()">pizza 40₪</button>`
-el01.innerHTML += `<button onClick="inputNumber()">hamburger 50₪</button>`
-el01.innerHTML += `<button onClick="inputNumber()">sushi 35₪</button>`
-el01.innerHTML += `<button onClick="inputNumber()">chicken 70₪</button>`
-el01.innerHTML += `<button onClick="inputNumber()">Eggs ₪20</button>`
-el01.innerHTML += `<button onClick="inputNumber()">milk ₪20</button>`
-
+if (el01) {
+    el01.innerHTML = `<h1> menu </h1>`
+    el01.innerHTML += `<button onClick="inputNumber()">pizza 40₪</button>`
+    el01.innerHTML += `<button onClick="inputNumber()">hamburger 50₪</button>`
+    el01.innerHTML += `<button onClick="inputNumber()">sushi 35₪</button>`
+    el01.innerHTML += `<button onClick="inputNumber()">chicken 70₪</button>`
+    el01.innerHTML += `<button onClick="inputNumber()">Eggs ₪20</button>`
+    el01.innerHTML += `<button onClick="inputNumber()">milk ₪20</button>`
+}
 
 
 var el02 = document.querySelector('.cube4')
-el02.innerHTML = '<h1>picture of dishes served in the restaurant</h1>'
+if (el02) el02.innerHTML = '<h1>picture of dishes served in the restaurant</h1>'
 
 var el02 = document.querySelector('.cube2')
-el02.innerHTML = '<h1>Pictures of the restaurant</h1>'
+if (el02) el02.innerHTML = '<h1>Pictures of the restaurant</h1>'
 
 var el03 = document.querySelector('.cube3')
-el03.innerHTML = '<h1>Other sites</h1>'
+if (el03) el03.innerHTML = '<h1>Other sites</h1>'
 
 var el04 = document.querySelector('.cube5')
-el04.innerHTML = '<h1>Comments</h1>'
+if (el04) el04.innerHTML = '<h1>Comments</h1>'
+
+var el05 = document.querySelector('.cube7')
+if (el05) el05.innerHTML = '<h1>krunker launcher</h1>'
 
 function popup() {
     console.log('hey');
