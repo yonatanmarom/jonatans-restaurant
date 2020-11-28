00dbd668887b34e20ef6a149f6db2643e6c435e7
yonatanmarom <yonatan.ma.100@gmail.com>
2020-11-28 20:26:37 +0200

    all

--- eee.js
+++ eee.js
@@ -1,12 +1,12 @@
 var el01 = document.querySelector('.cube1')
 if (el01) {
     el01.innerHTML = `<h1> menu </h1>`
-    el01.innerHTML += `<button onClick="inputNumber()">pizza 40₪</button>`
-    el01.innerHTML += `<button onClick="inputNumber()">hamburger 50₪</button>`
-    el01.innerHTML += `<button onClick="inputNumber()">sushi 35₪</button>`
-    el01.innerHTML += `<button onClick="inputNumber()">chicken 70₪</button>`
-    el01.innerHTML += `<button onClick="inputNumber()">Eggs ₪20</button>`
-    el01.innerHTML += `<button onClick="inputNumber()">milk ₪20</button>`
+    el01.innerHTML += `<button class ="ddd" onClick="inputNumber()">pizza 40₪</button>`
+    el01.innerHTML += `<button class ="ddd" onClick="inputNumber()">hamburger 50₪</button>`
+    el01.innerHTML += `<button class ="ddd" onClick="inputNumber()">sushi 35₪</button>`
+    el01.innerHTML += `<button class ="ddd" onClick="inputNumber()">chicken 70₪</button>`
+    el01.innerHTML += `<button class ="ddd" onClick="inputNumber()">Eggs ₪20</button>`
+    el01.innerHTML += `<button class ="ddd" onClick="inputNumber()">milk ₪20</button>`
 }
 
 
@@ -26,6 +26,19 @@ if (el04) el04.innerHTML = '<h1>Comments</h1>'
 
 var el05 = document.querySelector('.cube7')
 if (el05) el05.innerHTML = '<h1>krunker launcher</h1>'
+
+function inputNumber2() {
+    var a = prompt('enter the code');
+    if (a === '102030') {
+        window.open('vip', '_blank');
+    } else {
+        console.log('Wrong number');
+    }
+
+
+
+}
+
 
 function popup() {
     console.log('hey');
